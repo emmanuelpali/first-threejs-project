213745327fdf6ce21def26f335f567522e2220b9
emmanuelpali <palinendir@gmail.com>
2023-12-29 20:00:26 -0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,156 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui';
+import { FontLoader } from 'three/addons/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+//axes helper
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+/**
+ * fonts
+ */
+
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    'helvetiker_bold.typeface.json',
+    (font) => {
+        const textGeometry = new TextGeometry(
+            'Emmanuel Pali',
+            {
+                // font passed from the function
+                font: font,
+                size: 0.3,
+                height: 0.1,
+                curveSegments: 5,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 4
+            }
+        )
+        // textGeometry.computeBoundingBox()
+        // textGeometry.translate(
+        //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+        //     -(textGeometry.boundingBox.max.y - 0.02)* 0.5,
+        //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+        // )
+        textGeometry.center()
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture})
+        // textMaterial.wireframe = true
+        const text = new THREE.Mesh(textGeometry, material)
+        scene.add(text)
+        const circleGeometry = new THREE.CircleGeometry(0.5,16);
+       
+        for(let i = 0; i < 100; i++){
+const circle = new THREE.Mesh(circleGeometry, material)
+            circle.position.x = (Math.random() - 0.5) * 20
+            circle.position.y = (Math.random() - 0.5) * 20
+            circle.position.z = (Math.random() - 0.5) * 20
+            const scale = Math.random()
+            circle.scale.set(scale,scale, scale)
+            circle.d
+            scene.add(circle)
+            let ans = Math.random() * 11
+            console.log(ans)
+        }
+    }
+
+)
+
+/**
+ * Object
+ */
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
+
+// scene.add(cube)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
